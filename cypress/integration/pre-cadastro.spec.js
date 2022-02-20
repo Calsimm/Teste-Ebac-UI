d814c7a9f7c2482f35cf0e29e74bb918cf3858cd
Maria C Braulino <carol.braulino@hotmail.com>
2022-02-20 16:23:25 -0300

    atualizacao nos testes

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -5,16 +5,17 @@ describe('Funcionalidade Pré Cadastro', () => {
 
     beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-    });   
+    });
 
 
     it('Deve completar o pré cadastro com sucesso', () => {
         let nomeFaker = faker.name.firstName()
         let sobrenomeFaker = faker.name.lastName()
         let emailFaker = faker.internet.email(nomeFaker)
+        let senhaFaker = faker.internet.password()
 
         cy.get('#reg_email').type(emailFaker)
-        cy.get('#reg_password').type('teste@teste.com')
+        cy.get('#reg_password').type(faker.internet.password())
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
@@ -22,7 +23,18 @@ describe('Funcionalidade Pré Cadastro', () => {
         cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
 
-        cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+
+        //iniciar uma compra em produtos
+        cy.get('.woocommerce-MyAccount-navigation-link--orders > a').click()
+        cy.get('.woocommerce-Button').click()
+
     })
-    
+
+        
+
+
+
+
+
 });
