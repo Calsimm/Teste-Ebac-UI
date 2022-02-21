dea77f1c8dfe7dd428600c51523f62d127487889
Maria C Braulino <carol.braulino@hotmail.com>
2022-02-21 17:58:02 -0300

    usando massa de dados

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -4,7 +4,7 @@ var faker = require('faker-br');
 describe('Funcionalidade Pré Cadastro', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta/')
     });
 
 
